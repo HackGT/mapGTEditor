df1b54e32813039f6e4f38282889e1aed32812a8
Elton <eltonp3103@gmail.com>
2019-06-05 18:37:01 -0400

    created outline for polygon tool

---
+++ js/tools/polygon.js
@@ -0,0 +1,37 @@
+class Polygon extends Shape {
+    constructor() {
+
+    }
+
+    showNodes() {
+        
+    }
+
+    hideNodes() {
+
+    }
+
+    static onMouseClick(event) {
+
+    }
+
+    static onMouseMove(event) {
+
+    }
+
+    static onMouseDownNode(event) {
+
+    }
+
+    static onMouseMoveNode(event) {
+
+    }
+
+    static onMouseUpNode(event) {
+
+    }
+
+    static removeEventListeners() {
+
+    }
+}
