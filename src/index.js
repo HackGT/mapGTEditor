e633b1d8a285ed8ee274ecca50e8a745eeefb7bf
Elton <eltonp3103@gmail.com>
2019-09-30 16:24:45 -0400

    added watch option to webpack build command

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import { Canvas } from "./canvas/Canvas";
 import { SelectTool } from "./tools/SelectTool";
 import { RectangleTool } from "./tools/RectangleTool";
 import { PolygonTool } from "./tools/PolygonTool";
+// import { Tool } from "./tools/Tool";
 
 window.onload = () => {
     const canvas = new Canvas(1000, 500);
     const tools = {
         "select": new SelectTool(canvas),
         "rectangle": new RectangleTool(canvas),
-        "polygon": new PolygonTool(canvas)
+        "polygon": new PolygonTool(canvas),
+        "image-upload": new (canvas)
     }
 }
